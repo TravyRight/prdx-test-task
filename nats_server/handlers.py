c8f4488d664ec9c7a7d0fb2c867cc89d87a6b928
travyright (home) <timadubakov@gmail.com>
2025-02-11 22:13:11 +0500

    edit connect func

--- nats_server/handlers.py
+++ nats_server/handlers.py
@@ -9,7 +9,19 @@ from config import bot
 
 async def nats_message_handler(msg):
     channel_id = str(msg.subject).split(".")[1]
+    channel = await bot.fetch_channel(int(channel_id))
+
+    """
+    Изначально строка 12 выглядела так:
     channel = await bot.fetch_channel(channel_id)
+    
+    Удивительно, но этот код работал.
+    Я посмотрел код метода fetch_message и оказалось, 
+    что для получения канала можно передать channel_id
+    не только c типом int, но и с типом str. 
+    
+    Лучше добавить int() на случай, если разработчики disnake что-либо поменяют
+    """
 
     if channel is not None:
         await channel.send(content=msg.data.decode())
@@ -25,7 +37,7 @@ async def connect_to_nats_handler(max_retries=5, retry_delay=2):
             logging.info("Successful connection to NATS!")
             return nc
 
-        except (ErrTimeout, ErrNoServers) as e:
+        except Exception as e:  # (ErrTimeout, ErrNoServers)
             attempts += 1
             logging.error(f"Error connecting to NATS: {e}. Retry after {retry_delay} seconds...")
             await asyncio.sleep(retry_delay)
